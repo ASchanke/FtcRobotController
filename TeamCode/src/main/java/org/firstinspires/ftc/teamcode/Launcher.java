60a7378e67d61b68009b0f68ff0064a2fe30fb5b
Shua <lax.shua@gmail.com>
2020-11-25 19:53:10 -0500

    fixes on/off function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Launcher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Launcher.java
@@ -13,15 +13,21 @@ public class Launcher {
     public Launcher(Servo servo, DcMotor motor ) {
         this.servo = servo;
         this.motor = motor;
+        motor.setDirection(DcMotorSimple.Direction.FORWARD);
+    }
+    public void on() {
+        motor.setPower(1.0);
     }
 
     public void launch() {
-        motor.setDirection(DcMotorSimple.Direction.FORWARD);
-        motor.setPower(1.0);
         servo.setPosition(1);
         timer.reset();
         while(timer.milliseconds() < 2000){}
         servo.setPosition(0);
+
+    }
+
+    public void off() {
         motor.setPower(0.0);
     }
 
